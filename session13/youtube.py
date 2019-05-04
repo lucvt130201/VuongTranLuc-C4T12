9c0e60e2345fa17bf58c0ec025f130380c5d17a7
Vuong Luc <vuongtranluc@Macs-MacBook-Pro-2.local>
2019-05-05 00:47:44 +0700

    test git

--- session13/youtube.py
+++ session13/youtube.py
@@ -1,9 +1,27 @@
 from __future__ import unicode_literals
 import youtube_dl
 
-ydl_opts = {}
-# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#     ydl.download(['https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s'])
+video_info =[]
+video_link = ["https://www.youtube.com/watch?v=Wv2rLZmbPMA", "https://www.youtube.com/watch?v=GIDoQsQyS0s"]
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.extract_info(['https://www.youtube.com/watch?v=Wv2rLZmbPMA'])
+  
+
+for i in video_link:
+
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        r = ydl.extract_info(i, False)
+        video_info.append(r['title'])
+
+print(video_info)
+
+    
+# ydl_opts = {}
+# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    # ydl.download(['https://www.youtube.com/watch?v=pFSQh_5QE40&list=RDMMpFSQh_5QE40&start_radio=1'])
+# i = input("what song do u ưant to hear?")
+# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#     r = ydl.extract_info('https://www.youtube.com/watch?v=o_lN37OAJ9U', False)
+#     print(r['title'])
+
+
